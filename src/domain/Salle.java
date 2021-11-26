2b46ad4ea18b09388a133696a7b3bdcb076a231a
Paul Chauveau <chauveaupaul17@gmail.com>
2021-11-26 11:51:16 +0100

    Ajout de Horaire et du constructeur de Salle pour tester

--- src/domain/Salle.java
+++ src/domain/Salle.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //ValueObject
@@ -8,5 +9,13 @@ public class Salle {
 	private final List<Horaire> horaireSalle; //créer une classe horaire
 	private final int ouverture; //int ou date ou List ? (List car plusieurs dates d'ouverture sur la semaine ou le mois)
 	private final List<Evenement> programme; //programme de la salle
+	
+	public Salle(int capacite, int ouverture) {
+		// à modifier
+		this.capacite = capacite;
+		this.horaireSalle = new ArrayList<Horaire>();
+		this.ouverture = ouverture;
+		this.programme = new ArrayList<Evenement>();
+	}
 	
 }
